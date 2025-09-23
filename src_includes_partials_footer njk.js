ab607ab0b4bba72aa97d8a4d7f04629758e640cd
JollyHeart <levijohn2020@gmail.com>
2025-09-23 21:48:12 +0100

    Add files via upload

---
+++ src_includes_partials_footer njk.js
@@ -0,0 +1,7 @@
+src/_includes/partials/footer.njk
+
+<footer class="bg-blue-900 text-white py-6 mt-8">
+  <div class="container mx-auto text-center">
+    <p>&copy; {{ "now" | date("yyyy") }} Handfeetball. All Rights Reserved.</p>
+  </div>
+</footer>
